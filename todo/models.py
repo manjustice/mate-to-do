d4153b1f34e67db0021884b254718c8d86cd2ec7
manjustice <manjustice1998@gmail.com>
2023-04-03 10:20:16 +0300

    Added models to todo app

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_done = models.BooleanField(default=False)
+    tags = models.ManyToManyField("Tag", related_name="tasks")
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=63)
