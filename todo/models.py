6309138f991a2f8f341b588049c29165843c3520
manjustice <manjustice1998@gmail.com>
2023-04-03 12:58:52 +0300

    Changed migrations and models

--- todo/models.py
+++ todo/models.py
@@ -4,9 +4,19 @@ from django.db import models
 class Task(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(null=True)
     is_done = models.BooleanField(default=False)
     tags = models.ManyToManyField("Tag", related_name="tasks")
+
+    class Meta:
+        ordering = ["is_done", "-created_at"]
+
+    def __str__(self) -> str:
+        return self.content
 
 
 class Tag(models.Model):
     name = models.CharField(max_length=63)
+
+    def __str__(self) -> str:
+        return self.name
